adb56876fd3cb1b2431ef71edc62db35ca8c0f6c
Ruan Carlos <37166163+Ruan-CGM@users.noreply.github.com>
2025-01-22 08:31:06 -0300

    Subindo o projeto Streamlit FIFA

---
+++ pages/2_🤖_players.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Players",
+    page_icon="🤖",
+    layout="wide",
+)
+
+df_data = st.session_state["data"]
+
+clubes = df_data["Club"].unique()
+club = st.sidebar.selectbox("Clube", clubes)
+
+df_players = df_data[(df_data["Club"] == club)]
+players = df_players["Name"].unique()
+player = st.sidebar.selectbox("Jogador", players)
+
+players_stats = df_data[df_data["Name"] == player].iloc[0]
+
+st.image(players_stats["Photo"])
+st.title(players_stats["Name"])
+
+st.markdown(f"**Clube:** {players_stats['Club']}")
+st.markdown(f"**Nacionalidade:** {players_stats['Nationality']}")
+st.markdown(f"**Posição:** {players_stats['Position']}")
+
+col1, col2, col3, col4 = st.columns(4)
+col1.markdown(f"**Idade:** {players_stats['Age']}")
+col2.markdown(f"**Altura:** {players_stats['Height(cm.)'] / 100}")
+col3.markdown(f"**Peso:** {players_stats['Weight(lbs.)'] * 0.453:.2f}")
+st.divider()
+
+
+st.subheader(f"Overall {players_stats['Overall']}")
+st.progress(int(players_stats["Overall"]) / 100)
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric(label="Valor de Mercado", value=f"£ {players_stats['Value(£)']:.2f}")
+col2.metric(label="Remuneração Semanal", value=f"£ {players_stats['Wage(£)']:.2f}")
+col3.metric(label="Cláusula de rescisão", value=f"£ {players_stats['Release Clause(£)']:.2f}")
